f86793e02368690454225e5f544e7bcb47b8d728
Jesse Akorah <jesseakorah@gmail.com>
2023-01-19 21:18:37 +0100

    Set up server and connect to local database

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to Database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to Database.", err);
+  });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
