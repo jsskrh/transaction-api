5585e58a80aea754158dbd5ba9794dc60a93500b
Jesse Akorah <jesseakorah@gmail.com>
2023-01-20 03:19:45 +0100

    Handle deposit

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,5 +6,6 @@ const auth = require("../middleware/index");
 const Transactions = require("../controllers/transactions");
 
 router.post("/transfer", auth, Transactions.transfer);
+router.post("/deposit", auth, Transactions.deposit);
 
 module.exports = router;
